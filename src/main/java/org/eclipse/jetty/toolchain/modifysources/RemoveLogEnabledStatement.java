735231bd66563274c964f6e20e539dc58f549fb5
olivier lamy <olamy@apache.org>
2018-05-11 10:24:58 +1000

    first commit
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ src/main/java/org/eclipse/jetty/toolchain/modifysources/RemoveLogEnabledStatement.java
@@ -0,0 +1,107 @@
+package org.eclipse.jetty.toolchain.modifysources;
+
+
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
+import com.github.javaparser.utils.SourceRoot;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Remove if block starting with if(LOG.isDebugEnabled() from java sources
+ */
+@Mojo( name = "remove-log-enabled", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true )
+public class RemoveLogEnabledStatement
+    extends AbstractMojo
+{
+    /**
+     * Location of the sources.
+     */
+    @Parameter( defaultValue = "${project.basedir}/src/main/java", property = "sourcesLocation", required = true )
+    private File sourcesLocation;
+
+    /**
+     * Location of the modified sources.
+     */
+    @Parameter( defaultValue = "${project.build.directory}/modified-sources", property = "outputLocation", required = true )
+    private File outputDirectory;
+
+    /**
+     * Maven Project.
+     */
+    @Component
+    protected MavenProject project;
+
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        try
+        {
+            SourceRoot sourceRoot = new SourceRoot( sourcesLocation.toPath() );
+
+            // Our sample is in the root of this directory, so no package name.
+            List<ParseResult<CompilationUnit>> parseResults = sourceRoot.tryToParse( "" );
+
+            parseResults.stream().forEach(
+                compilationUnitParseResult -> compilationUnitParseResult.getResult().get().accept(
+                    new ModifierVisitor<Void>()
+                    {
+                        @Override
+                        public Visitable visit( IfStmt n, Void arg )
+                        {
+                            if ( n.getCondition().toString().endsWith( "isDebugEnabled()" ) )
+                            {
+                                n.remove();
+                            }
+                            return super.visit( n, arg );
+                        }
+                        // TODO configurable option
+                        //                    @Override
+                        //                    public Visitable visit( ExpressionStmt n, Void arg )
+                        //                    {
+                        //                        if(n.getExpression().toString().contains( ".warn(") //
+                        //                            || n.getExpression().toString().contains( ".info(") //
+                        //                            || n.getExpression().toString().contains( ".ignore("))
+                        //                        {
+                        //                            n.remove();
+                        //                            return null;
+                        //                        } else
+                        //                        {
+                        //                            return super.visit( n, arg );
+                        //                        }
+                        //                    }
+                    }, null ) );
+
+            Path out = outputDirectory.toPath();
+            if ( Files.exists( out ) )
+            {
+                Files.walk( out ).sorted( Comparator.reverseOrder() ).map( Path::toFile ).forEach( File::delete );
+            }
+
+            Files.createDirectories( out );
+            sourceRoot.saveAll( out );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "fail to modifyt jetty sources", e );
+        }
+
+
+    }
+}
---
+++ src/test/java/org/eclipse/jetty/toolchain/modifysources/RemoveLogEnabledStatementTest.java
@@ -0,0 +1,60 @@
+package org.eclipse.jetty.toolchain.modifysources;
+
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class RemoveLogEnabledStatementTest
+{
+    @Rule
+    public MojoRule rule = new MojoRule()
+    {
+        @Override
+        protected void before()
+            throws Throwable
+        {
+        }
+
+        @Override
+        protected void after()
+        {
+        }
+    };
+
+    /**
+     * @throws Exception if any
+     */
+    @Test
+    public void testSomething()
+        throws Exception
+    {
+        File pom = new File( "target/test-classes/project-to-test/" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        RemoveLogEnabledStatement removeLogEnabledStatement =
+            (RemoveLogEnabledStatement) rule.lookupConfiguredMojo( pom, "remove-log-enabled" );
+        assertNotNull( removeLogEnabledStatement );
+        removeLogEnabledStatement.execute();
+
+        File outputDirectory = (File) rule.getVariableValueFromObject( removeLogEnabledStatement, "outputDirectory" );
+        assertNotNull( outputDirectory );
+        assertTrue( outputDirectory.exists() );
+
+        Path modified = Paths.get( outputDirectory.toString(), "org", "jetty", "Scanner.java" );
+
+        assertTrue( Files.exists(modified) );
+
+    }
+
+}
+
