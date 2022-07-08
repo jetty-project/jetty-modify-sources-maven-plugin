276f88ee2385c828583feeb61bb7e05956d64972
Olivier Lamy <olamy@apache.org>
2022-07-08 16:08:47 +1000

    avoid NPE and cleanup imports
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/org/eclipse/jetty/toolchain/modifysources/ModifyEE9ToEE8ServiceLoaderFiles.java
+++ src/main/java/org/eclipse/jetty/toolchain/modifysources/ModifyEE9ToEE8ServiceLoaderFiles.java
@@ -1,37 +1,5 @@
 package org.eclipse.jetty.toolchain.modifysources;
 
-
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.PackageDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.comments.JavadocComment;
-import com.github.javaparser.ast.expr.CastExpr;
-import com.github.javaparser.ast.expr.ClassExpr;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.FieldAccessExpr;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.expr.StringLiteralExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.modules.ModuleDeclaration;
-import com.github.javaparser.ast.modules.ModuleExportsDirective;
-import com.github.javaparser.ast.modules.ModuleOpensDirective;
-import com.github.javaparser.ast.modules.ModuleProvidesDirective;
-import com.github.javaparser.ast.modules.ModuleRequiresDirective;
-import com.github.javaparser.ast.modules.ModuleUsesDirective;
-import com.github.javaparser.ast.nodeTypes.NodeWithName;
-import com.github.javaparser.ast.nodeTypes.NodeWithType;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.github.javaparser.ast.visitor.ModifierVisitor;
-import com.github.javaparser.ast.visitor.Visitable;
-import com.github.javaparser.utils.SourceRoot;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -44,15 +12,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.eclipse.jetty.toolchain.modifysources.ModifyEE9ToEE8.changeEE9TypeToEE8;
 
 /**
- * Modify sources from EE9 project to be compiled with EE8 dependencies
+ * Modify services files from EE9 project to be compiled with EE8 dependencies
  */
 @Mojo( name = "modify-service-loader-files-ee9-to-ee8", defaultPhase = LifecyclePhase.PROCESS_RESOURCES, threadSafe = true )
 public class ModifyEE9ToEE8ServiceLoaderFiles
@@ -89,6 +55,9 @@ public class ModifyEE9ToEE8ServiceLoaderFiles
             {
                 String[] files = metaInfDirectory.list((dir, name) -> StringUtils.startsWith(name, "org.eclipse.jetty.ee9") ||
                         StringUtils.startsWith(name, "jakarta.servlet"));
+                if (files==null) {
+                    return;
+                }
                 for (String fileName : files) {
                     File file = new File(metaInfDirectory, fileName);
                     String newFileName = changeEE9TypeToEE8(fileName);
